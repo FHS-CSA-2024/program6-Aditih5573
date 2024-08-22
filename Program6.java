96a9d686e7f7c143b1a0fee934cd31ceceff2e7b
Aditi <109041004+Aditih5573@users.noreply.github.com>
2024-08-21 19:46:09 -0700

    Update Program6.java

--- Program6.java
+++ Program6.java
@@ -19,12 +19,25 @@ public class circleComponents {
  double area = (PI) * rad * rad;
  double circum = 2 * PI * rad;
  
+ double roundDiameter = Math.round(diameter * 1000.0)/1000.0;
+ double roundArea = Math.round(area * 1000.0)/1000.0;
+ double roundCircum = Math.round(circum * 1000.0)/1000.0;
+ 
+ 
  System.out.println("The radius of the circle = " + rad);
- System.out.println("The diameter of the circle = " + diameter);
- System.out.println("The area of the circle = " + area);
- System.out.println("The circumference of the circle = " + circum);
+ System.out.println("The diameter of the circle = " + roundDiameter);
+ System.out.println("The area of the circle = " + roundArea);
+ System.out.println("The circumference of the circle = " + roundCircum);
  
  
         
     }
 }
+
+//output
+//Enter the radius: 
+/*3.712
+The radius of the circle = 3.712
+The diameter of the circle = 7.424
+The area of the circle = 43.288
+The circumference of the circle = 23.323*/
